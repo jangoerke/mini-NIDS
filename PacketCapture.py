2b2964a1bdd16224650036181c7c0c221b245748
Jan Görke <jangoerke@proton.me>
2026-06-13 17:24:04 +0200

    PacketCapture.py Bugfix + main capture loop

--- PacketCapture.py
+++ PacketCapture.py
@@ -12,15 +12,16 @@ class PacketCapture:
         if IP in pkt and TCP in pkt:
             self.packet_queue.put(pkt)
     
-    def start_capture(self, interface='eth0'):
+    def start_capture(self, interface='enp8s0'):
         def capture_thread():
+            print("start sniffing")
             sniff(iface=interface,
                   prn=self.packet_callback,
                   store=0,
                   stop_filter=lambda _: self.stop_capture.is_set())
             
-            self.capture_thread = threading.Thread(target=capture_thread)
-            self.capture_thread.start()
+        self.capture_thread = threading.Thread(target=capture_thread)
+        self.capture_thread.start()
             
     
     def end_capture(self):
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import PacketCapture
+import queue
+
 def main():
-    print("Hello from mini-nids!")
+    pkt_capture = PacketCapture.PacketCapture()
+    pkt_capture.start_capture()
+    while True:
+        try:
+            pkt = pkt_capture.packet_queue.get(timeout=1)
+            print(pkt.summary())
+        except queue.Empty:
+            continue
+        except KeyboardInterrupt:
+            pkt_capture.stop_capture()
+            break
 
 
 if __name__ == "__main__":
