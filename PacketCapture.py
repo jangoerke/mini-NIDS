18af96d81a0852fddc90023d8ce5eb8d9c3a8a01
Jan Görke <jangoerke@proton.me>
2026-06-13 15:35:19 +0200

    implemented PacketCapture

--- PacketCapture.py
+++ PacketCapture.py
@@ -5,4 +5,24 @@ import queue
 
 class PacketCapture:
     def __init__(self):
-        pass
+        self.packet_queue = queue.Queue()
+        self.stop_capture = threading.Event()
+    
+    def packet_callback(self,pkt):
+        if IP in pkt and TCP in pkt:
+            self.packet_queue.put(pkt)
+    
+    def start_capture(self, interface='eth0'):
+        def capture_thread():
+            sniff(iface=interface,
+                  prn=self.packet_callback,
+                  store=0,
+                  stop_filter=lambda _: self.stop_capture.is_set())
+            
+            self.capture_thread = threading.Thread(target=capture_thread)
+            self.capture_thread.start()
+            
+    
+    def end_capture(self):
+        self.stop_capture.set()
+        self.capture_thread.join()
