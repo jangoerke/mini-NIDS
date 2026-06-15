1a44e166f7773ef0ef1418d82953bfddf48eebfa
Jan <jangoerke@proton.me>
2026-06-15 17:08:41 +0200

    added analyze_pkt()

--- TrafficAnalyzer.py
+++ TrafficAnalyzer.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from scapy.all import IP, TCP
 
 class TrafficAnalyzer:
     def __init__(self):
@@ -8,3 +9,22 @@ class TrafficAnalyzer:
             'start_time': None,
             'end_time': None
         })
+    
+    def analyze_pkt(self, pkt):
+        if IP in pkt and TCP in pkt:
+            source_ip = pkt[IP].src
+            dest_ip = pkt[IP].dst
+            source_port = pkt[TCP].sport
+            dest_port = pkt[TCP].dport
+
+            flow_key = (source_ip,source_port,dest_ip,dest_port)
+
+            stats = self.flow_stats[flow_key]
+            stats['packer_count'] += 1
+            stats['bin_length'] += len(pkt)
+
+            current_time = pkt.time
+
+            if stats['start_time'] is None:
+                stats['start_time'] = current_time
+            stats['end_time'] = current_time
